84a12fa372564247a68f042256c1447112b45336
heimanba <yuankun.yk@alibaba-inc.com>
2022-09-17 15:45:01 +0800

    init project

---
+++ src/code/src/components/Toast.tsx
@@ -0,0 +1,59 @@
+import { useCallback, useEffect, useRef } from "react";
+import { close } from "./icons";
+import "./Toast.scss";
+import { ToolButton } from "./ToolButton";
+
+const DEFAULT_TOAST_TIMEOUT = 5000;
+
+export const Toast = ({
+  message,
+  clearToast,
+  closable = false,
+  // To prevent autoclose, pass duration as Infinity
+  duration = DEFAULT_TOAST_TIMEOUT,
+}: {
+  message: string;
+  clearToast: () => void;
+  closable?: boolean;
+  duration?: number;
+}) => {
+  const timerRef = useRef<number>(0);
+  const shouldAutoClose = duration !== Infinity;
+  const scheduleTimeout = useCallback(() => {
+    if (!shouldAutoClose) {
+      return;
+    }
+    timerRef.current = window.setTimeout(() => clearToast(), duration);
+  }, [clearToast, duration, shouldAutoClose]);
+
+  useEffect(() => {
+    if (!shouldAutoClose) {
+      return;
+    }
+    scheduleTimeout();
+    return () => clearTimeout(timerRef.current);
+  }, [scheduleTimeout, message, duration, shouldAutoClose]);
+
+  const onMouseEnter = shouldAutoClose
+    ? () => clearTimeout(timerRef?.current)
+    : undefined;
+  const onMouseLeave = shouldAutoClose ? scheduleTimeout : undefined;
+  return (
+    <div
+      className="Toast"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <p className="Toast__message">{message}</p>
+      {closable && (
+        <ToolButton
+          icon={close}
+          aria-label="close"
+          type="icon"
+          onClick={clearToast}
+          className="close"
+        />
+      )}
+    </div>
+  );
+};
